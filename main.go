8085351225d801492d324eb58768dd74a5022097
Hidetake Iwata <int128@gmail.com>
2023-01-19 10:54:30 +0900

    kubebuilder create api --group googlecloudpubsuboperator --version v1 --kind Subscription

--- main.go
+++ main.go
@@ -112,6 +112,13 @@ func main() {
 		setupLog.Error(err, "unable to create controller", "controller", "Topic")
 		os.Exit(1)
 	}
+	if err = (&controllers.SubscriptionReconciler{
+		Client: mgr.GetClient(),
+		Scheme: mgr.GetScheme(),
+	}).SetupWithManager(mgr); err != nil {
+		setupLog.Error(err, "unable to create controller", "controller", "Subscription")
+		os.Exit(1)
+	}
 	//+kubebuilder:scaffold:builder
 
 	if err := mgr.AddHealthzCheck("healthz", healthz.Ping); err != nil {
