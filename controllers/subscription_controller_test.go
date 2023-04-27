18bfb3f1e2786c2f88a4c82cdd999ed8ed04c650
44smkn <kenji.shimizu@quipper.com>
2023-04-27 15:01:56 +0900

    Add test for subscription controller

---
+++ controllers/subscription_controller_test.go
@@ -0,0 +1,60 @@
+package controllers
+
+import (
+	"context"
+	"time"
+
+	"cloud.google.com/go/pubsub"
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+	"google.golang.org/api/option"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+
+	googlecloudpubsuboperatorv1 "github.com/quipper/google-cloud-pubsub-operator/api/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	//+kubebuilder:scaffold:imports
+)
+
+var _ = Describe("Subscription controller", func() {
+	Context("When creating a Topic resource", func() {
+		It("Should create a Pub/Sub Subscription", func(ctx context.Context) {
+			psClient, err := pubsub.NewClient(ctx, "my-project",
+				option.WithEndpoint(psServer.Addr),
+				option.WithoutAuthentication(),
+				option.WithGRPCDialOption(grpc.WithTransportCredentials(insecure.NewCredentials())),
+			)
+			Expect(err).ShouldNot(HaveOccurred())
+
+			topicID := "my-topic"
+			_, err = psClient.CreateTopic(ctx, topicID)
+			Expect(err).ShouldNot(HaveOccurred())
+
+			By("Creating a Subscription")
+			topic := &googlecloudpubsuboperatorv1.Subscription{
+				TypeMeta: metav1.TypeMeta{
+					APIVersion: "googlecloudpubsuboperator.quipper.github.io/v1",
+					Kind:       "Subscription",
+				},
+				ObjectMeta: metav1.ObjectMeta{
+					Name:      "example",
+					Namespace: "default",
+				},
+				Spec: googlecloudpubsuboperatorv1.SubscriptionSpec{
+					SubscriptionProjectID: "my-project",
+					SubscriptionID:        "my-subscription",
+					TopicProjectID:        "my-project",
+					TopicID:               topicID,
+				},
+			}
+			Expect(k8sClient.Create(ctx, topic)).Should(Succeed())
+
+			By("Checking if the Subscription exists")
+			Eventually(func(g Gomega) {
+				subscriptionExists, err := psClient.Subscription("my-subscription").Exists(ctx)
+				g.Expect(err).ShouldNot(HaveOccurred())
+				g.Expect(subscriptionExists).Should(BeTrue())
+			}, 3*time.Second, 100*time.Millisecond).Should(Succeed())
+		})
+	})
+})
